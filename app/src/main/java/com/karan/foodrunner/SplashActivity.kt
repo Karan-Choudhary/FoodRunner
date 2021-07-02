8fd2c872a1019d39f7c93ad972996a589df49235
KARAN CHOUDHARY <54716931+Karan-Choudhary@users.noreply.github.com>
2021-07-02 12:07:01 +0530

    Initial Commit with Splash Screen

---
+++ app/src/main/java/com/karan/foodrunner/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.karan.foodrunner
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val startAct = Intent(this,MainActivity::class.java)
+            finish()
+            startActivity(startAct)
+        },1000)
+    }
+}

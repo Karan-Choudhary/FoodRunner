29a6cdf6c8172b59080928c8312c5369910256f1
KARAN CHOUDHARY <54716931+Karan-Choudhary@users.noreply.github.com>
2021-07-02 15:05:48 +0530

    Login Functionality Added

--- app/src/main/java/com/karan/foodrunner/Activity/LoginActivity.kt
+++ app/src/main/java/com/karan/foodrunner/Activity/LoginActivity.kt
@@ -1,13 +1,26 @@
 package com.karan.foodrunner.Activity
 
+import android.app.Activity
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.cardview.widget.CardView
+import androidx.core.app.ActivityCompat
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.RequestFuture
 import com.android.volley.toolbox.Volley
 import com.karan.foodrunner.R
+import com.karan.foodrunner.util.ConnectionManager
+import org.json.JSONObject
 
 class LoginActivity : AppCompatActivity() {
 
@@ -18,10 +31,23 @@ class LoginActivity : AppCompatActivity() {
     lateinit var txtForgetPassword:TextView
     lateinit var txtGoToSignUp:TextView
     lateinit var btnLogin:Button
+    lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        sharedPreferences = getSharedPreferences(getString(R.string.preference_file_name),
+            MODE_PRIVATE)
+
+        var isLoggedIn = sharedPreferences.getBoolean("isLoggedIn",false)
         setContentView(R.layout.activity_login)
+
+        if(isLoggedIn)
+        {
+            var i1 = Intent(this,MainActivity::class.java)
+            startActivity(i1)
+            finish()
+        }
 
         etMobileNumber = findViewById(R.id.etMobileNumber)
         etPassword = findViewById(R.id.etPassword)
@@ -29,11 +55,88 @@ class LoginActivity : AppCompatActivity() {
         txtForgetPassword = findViewById(R.id.txtForgetPassword)
         txtGoToSignUp = findViewById(R.id.txtGoToSignUp)
 
+        val phoneNum:String? = etMobileNumber.text.toString()
+        val pass:String? = etPassword.text.toString()
+
         val queue = Volley.newRequestQueue(this)
         val url = "http://13.235.250.119/v2/login/fetch_result/"
 
+        btnLogin.setOnClickListener{
+
+            val jsonParams = JSONObject()
+            jsonParams.put("mobile_number",phoneNum)
+            jsonParams.put("password",pass)
 
 
+            if(ConnectionManager().checkConnectivity(this))
+            {
+                val jsonObjectRequest = object: JsonObjectRequest(Request.Method.POST,url,jsonParams,Response.Listener {
+
+                    try {
+                        val data = it.getJSONObject("data")
+                        val success = data.getBoolean("success")
+
+                        if(success)
+                        {
+                            val mainData = data.getJSONObject("data")
+                            savePreferences(
+                                mainData.getString("user_id"),
+                                mainData.getString("name"),
+                                mainData.getString("email"),
+                                mainData.getString("mobile_number"),
+                                mainData.getString("address"))
+
+                            val intent = Intent(this,MainActivity::class.java)
+                            finish()
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this, "INVALID Credentials", Toast.LENGTH_SHORT).show()
+                        }
+                    } catch (e : Exception){
+                        Toast.makeText(this, "Some Error Occurred!!!", Toast.LENGTH_SHORT).show()
+                    }
+
+                }, Response.ErrorListener {
+                    Toast.makeText(this, "Some Error Occurred, Please try again Later(VolleyError)", Toast.LENGTH_SHORT).show()
+                }){
+                    override fun getHeaders(): MutableMap<String, String> {
+                        val headers = HashMap<String,String>()
+                        headers["Content-type"] = "application/json"
+                        headers["token"] = "957582bfd2ec65"
+                        return headers
+                    }
+                }
+                queue.add(jsonObjectRequest)
+            } else{
+                val dialog = AlertDialog.Builder(this)
+                dialog.setTitle("Error")
+                dialog.setMessage("Internet Connection is not Found")
+                dialog.setPositiveButton("Open Settings"){_,_ ->
+                    val settingIntent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
+                    startActivity(settingIntent)
+                    this?.finish()
+                }
+
+                dialog.setNegativeButton("Exit"){_,_ ->
+                    ActivityCompat.finishAffinity(this as Activity)
+                }
+                dialog.create()
+                dialog.show()
+            }
+
+        }
 
     }
+
+    private fun savePreferences(user_id:String ,name:String, email:String, mobile_number:String, address:String) {
+
+        sharedPreferences.edit().putBoolean("isLoggedIn",true).apply()
+        sharedPreferences.edit().putString("user_id",user_id).apply()
+        sharedPreferences.edit().putString("name",name).apply()
+        sharedPreferences.edit().putString("email",email).apply()
+        sharedPreferences.edit().putString("mobile_number",mobile_number).apply()
+        sharedPreferences.edit().putString("address",address).apply()
+
+    }
+
 }

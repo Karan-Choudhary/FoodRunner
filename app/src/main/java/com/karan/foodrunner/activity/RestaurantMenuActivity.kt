18cd2596018929272c81e1d66c57f4cf5bfe8707
KARAN CHOUDHARY <54716931+Karan-Choudhary@users.noreply.github.com>
2021-07-08 12:01:17 +0530

    Create RestaurantMenuActivity.kt

---
+++ app/src/main/java/com/karan/foodrunner/activity/RestaurantMenuActivity.kt
@@ -0,0 +1,12 @@
+package com.karan.foodrunner.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.karan.foodrunner.R
+
+class RestaurantMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurant_menu)
+    }
+}

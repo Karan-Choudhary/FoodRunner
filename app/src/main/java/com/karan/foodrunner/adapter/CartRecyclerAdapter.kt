9843191b7427e9a506dc2bbc41d3899ba5340162
KARAN CHOUDHARY <54716931+Karan-Choudhary@users.noreply.github.com>
2021-07-09 10:59:06 +0530

    CartRecyclerAdapter class Added

---
+++ app/src/main/java/com/karan/foodrunner/adapter/CartRecyclerAdapter.kt
@@ -0,0 +1,36 @@
+package com.karan.foodrunner.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.karan.foodrunner.R
+import com.karan.foodrunner.model.CartItems
+
+class CartRecyclerAdapter(val context: Context, val cartItems : ArrayList<CartItems>) : RecyclerView.Adapter<CartRecyclerAdapter.CartViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_cart_single_row,parent,false)
+        return CartViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CartViewHolder, position: Int) {
+        val cartItem = cartItems[position]
+        holder.txtOrderItem.text = cartItem.itemName
+        holder.txtOrderItemPrice.text = "Rs. ${cartItem.itemPrice}"
+    }
+
+    override fun getItemCount(): Int {
+        return cartItems.size
+    }
+    
+    class CartViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    {
+        val txtOrderItem : TextView = view.findViewById(R.id.txtOrderItem)
+        val txtOrderItemPrice:TextView = view.findViewById(R.id.txtOrderItemPrice)
+    }
+
+
+}

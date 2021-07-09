a9ce3c8b10a15d8bb483b10c9e935385788ff343
KARAN CHOUDHARY <54716931+Karan-Choudhary@users.noreply.github.com>
2021-07-09 14:29:37 +0530

    Update OrderPlacedActivity.kt

--- app/src/main/java/com/karan/foodrunner/activity/OrderPlacedActivity.kt
+++ app/src/main/java/com/karan/foodrunner/activity/OrderPlacedActivity.kt
@@ -1,12 +1,31 @@
 package com.karan.foodrunner.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.karan.foodrunner.R
+import com.karan.foodrunner.fragment.HomeFragment
 
 class OrderPlacedActivity : AppCompatActivity() {
+
+    lateinit var btnOk : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_placed)
+
+        btnOk = findViewById(R.id.btnOK)
+
+        btnOk.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finishAffinity()
+        }
     }
+
+    override fun onBackPressed() {
+        // user can't go back
+    }
+
 }

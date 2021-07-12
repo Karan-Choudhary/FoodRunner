73d695628d20e0b35211bf910ae4249a400e4034
KARAN CHOUDHARY <54716931+Karan-Choudhary@users.noreply.github.com>
2021-07-12 11:41:21 +0530

    Update MyProfileFragment.kt
    Project Content on profile fragment from shared preferences file

--- app/src/main/java/com/karan/foodrunner/fragment/MyProfileFragment.kt
+++ app/src/main/java/com/karan/foodrunner/fragment/MyProfileFragment.kt
@@ -1,20 +1,48 @@
 package com.karan.foodrunner.fragment
 
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.karan.foodrunner.R
 
 
 class MyProfileFragment : Fragment() {
+
+    lateinit var txtName:TextView
+    lateinit var txtMobileNumber:TextView
+    lateinit var txtEmail:TextView
+    lateinit var txtAddress:TextView
+    lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_my_profile, container, false)
+
+        txtName = view.findViewById(R.id.txtName)
+        txtMobileNumber = view.findViewById(R.id.txtMobileNumber)
+        txtEmail = view.findViewById(R.id.txtEmail)
+        txtAddress = view.findViewById(R.id.txtAddress)
+
+        txtName.text = sharedPreferences.getString("name","User")
+        txtMobileNumber.text = sharedPreferences.getString("mobile_number","")
+        txtEmail.text = sharedPreferences.getString("email","")
+        txtAddress.text = sharedPreferences.getString("address","")
+
+        return view
+    }
+
+    override fun onAttach(context: Context) {
+        sharedPreferences = context.getSharedPreferences(getString(R.string.preference_file_name), MODE_PRIVATE)
+        super.onAttach(context)
+
     }
 }

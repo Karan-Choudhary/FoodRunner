c356d6fac6cf9951433b13fcbdd726fbdcc1ac8e
KARAN CHOUDHARY <54716931+Karan-Choudhary@users.noreply.github.com>
2021-07-12 14:18:11 +0530

    Create FavouriteRecyclerAdapter

--- app/src/main/java/com/karan/foodrunner/adapter/FavouriteRecyclerAdapter.kt
+++ app/src/main/java/com/karan/foodrunner/adapter/FavouriteRecyclerAdapter.kt
@@ -17,32 +17,53 @@ import com.karan.foodrunner.R
 import com.karan.foodrunner.activity.RestaurantMenuActivity
 import com.karan.foodrunner.database.RestaurantDataBase
 import com.karan.foodrunner.database.RestaurantEntity
-import com.karan.foodrunner.model.Restaurant
-import com.karan.foodrunner.model.RestaurantMenu
 import com.squareup.picasso.Picasso
 
-class HomeRecyclerAdapter(val context: Context, private var itemList:ArrayList<Restaurant>) :RecyclerView.Adapter<HomeRecyclerAdapter.HomeViewHolder>(){
+class FavouriteRecyclerAdapter(val context: Context,private var itemList:List<RestaurantEntity>) :RecyclerView.Adapter<FavouriteRecyclerAdapter.FavouriteViewHolder>(){
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_home_single_row,parent,false)
-        return HomeViewHolder(view)
+
+    class FavouriteViewHolder(view : View) : RecyclerView.ViewHolder(view)
+    {
+        val txtFoodName: TextView = view.findViewById(R.id.txtFoodName)
+        val txtFoodPrice: TextView = view.findViewById(R.id.txtFoodPrice)
+        val txtFoodRating: TextView = view.findViewById(R.id.txtFoodRating)
+        val imgFoodImage: ImageView = view.findViewById(R.id.imgRecyclerRowProfileImage)
+        val llContent: LinearLayout = view.findViewById(R.id.llContent)
+        val txtFav: TextView = view.findViewById(R.id.txtFav)
     }
 
-    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavouriteViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_home_single_row,parent,false)
+        return FavouriteViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: FavouriteViewHolder, position: Int) {
         val restaurant = itemList[position]
         val restaurantEntity = RestaurantEntity(restaurant.id,restaurant.name,restaurant.rating,restaurant.cost_for_one,restaurant.image_url)
 
         holder.txtFoodName.tag = restaurant.id+""
         holder.txtFoodName.text = restaurant.name
         holder.txtFoodRating.text = restaurant.rating
-        holder.txtFoodPrice.text = restaurant.cost_for_one +"/Person"
-//        holder.imgFoodImage.setImageResource(food.foodImage)
+        holder.txtFoodPrice.text = restaurant.cost_for_one+"/Person"
+
+        holder.txtFav.tag = "liked"
+        holder.txtFav.background = context.resources.getDrawable(R.drawable.ic_fav_filled)
+
         Picasso.get().load(restaurant.image_url).error(R.drawable.ic_default_image_restaurant).into(holder.imgFoodImage)
 
-        //Adding and removing from favourite using Database
+
+        holder.llContent.setOnClickListener{
+            println(holder.txtFoodName.tag.toString())
+            val intent = Intent(context as Activity, RestaurantMenuActivity::class.java)
+            intent.putExtra("restaurantId",holder.txtFoodName.tag.toString())
+            intent.putExtra("restaurantName",holder.txtFoodName.text.toString())
+            context.startActivity(intent)
+        }
+
         holder.txtFav.setOnClickListener {
-            if(!DBAsyncTask(context,restaurantEntity,1).execute().get()){
-                val result = DBAsyncTask(context,restaurantEntity,2).execute().get()
+            if(!HomeRecyclerAdapter.DBAsyncTask(context, restaurantEntity, 1).execute().get()){
+                val result = HomeRecyclerAdapter.DBAsyncTask(context, restaurantEntity, 2).execute().get()
 
                 if(result)
                 {
@@ -53,7 +74,7 @@ class HomeRecyclerAdapter(val context: Context, private var itemList:ArrayList<R
                     Toast.makeText(context as Activity, "Some Error Occurred", Toast.LENGTH_SHORT).show()
                 }
             } else{
-                val result = DBAsyncTask(context,restaurantEntity,3).execute().get()
+                val result = HomeRecyclerAdapter.DBAsyncTask(context, restaurantEntity, 3).execute().get()
                 if(result)
                 {
                     Toast.makeText(context, "${restaurant.name} removed from Favourite", Toast.LENGTH_SHORT).show()
@@ -64,54 +85,32 @@ class HomeRecyclerAdapter(val context: Context, private var itemList:ArrayList<R
                 }
             }
         }
-        
-        holder.llContent.setOnClickListener{
-            println(holder.txtFoodName.tag.toString())
-            val intent = Intent(context as Activity,RestaurantMenuActivity::class.java)
-            intent.putExtra("restaurantId",holder.txtFoodName.tag.toString())
-            intent.putExtra("restaurantName",holder.txtFoodName.text.toString())
-            context.startActivity(intent)
-        }
     }
 
     override fun getItemCount(): Int {
         return itemList.size
     }
 
-    class HomeViewHolder(view: View) : RecyclerView.ViewHolder(view)
-    {
-        val txtFoodName:TextView = view.findViewById(R.id.txtFoodName)
-        val txtFoodPrice:TextView = view.findViewById(R.id.txtFoodPrice)
-        val txtFoodRating:TextView = view.findViewById(R.id.txtFoodRating)
-        val imgFoodImage:ImageView = view.findViewById(R.id.imgRecyclerRowProfileImage)
-        val llContent:LinearLayout = view.findViewById(R.id.llContent)
-        val txtFav:TextView = view.findViewById(R.id.txtFav)
-    }
-
-    fun filterList(filteredList: ArrayList<Restaurant>){
-        itemList = filteredList
-        notifyDataSetChanged()
-    }
-
-    class DBAsyncTask(val context:Context, private val restaurantEntity: RestaurantEntity, val mode:Int):AsyncTask<Void,Void,Boolean>(){
+    class DBAsyncTask(val context: Context, private val restaurantEntity: RestaurantEntity,private val mode:Int) : AsyncTask<Void,Void,Boolean>(){
         /*
         Mode 1 -> Check DB if the restaurant is fav or not
         Mode 2 -> Save the restaurant into DB as fav
         Mode 3 -> Remove the fav restaurant
          */
 
-        val db = Room.databaseBuilder(context,RestaurantDataBase::class.java,"restaurant-db").build()
         override fun doInBackground(vararg params: Void?): Boolean {
+            val db = Room.databaseBuilder(context,RestaurantDataBase::class.java,"restaurant=db").build()
+
             when(mode)
             {
                 1 -> {
                     //Check DB if the restaurant is fav or not
-                    val restaurant:RestaurantEntity? = db.restaurantDao().getRestaurantById(restaurantEntity.id.toString())
+                    val restaurant:RestaurantEntity?=db.restaurantDao().getRestaurantById(restaurantEntity.id.toString())
                     db.close()
                     return restaurant != null
                 }
                 2 -> {
-                    //Save the restaurant into DB as fav
+                    //Save restaurant into DB as Fav
                     db.restaurantDao().insertRestaurant(restaurantEntity)
                     db.close()
                     return true

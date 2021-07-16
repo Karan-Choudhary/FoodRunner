d90e6a7ac12db23dfcaa2c6395d20983561b66a2
KARAN CHOUDHARY <54716931+Karan-Choudhary@users.noreply.github.com>
2021-07-16 12:50:22 +0530

    disable dark mode

--- app/src/main/java/com/karan/foodrunner/activity/SplashActivity.kt
+++ app/src/main/java/com/karan/foodrunner/activity/SplashActivity.kt
@@ -4,12 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatDelegate
 import com.karan.foodrunner.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
 
         Handler().postDelayed({
             val startAct = Intent(this, LoginActivity::class.java)
